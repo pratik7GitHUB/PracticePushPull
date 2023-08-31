31f17f3bde556c84cafab775345ea9f3021c0e14
Dell <Dell@Alekh.bbrouter>
2023-08-31 23:56:01 +0530

    updated

--- DemoProject/src/main/java/Demo1.java
+++ DemoProject/src/main/java/Demo1.java
@@ -3,6 +3,6 @@ public class Demo1 {
 public static void main(String[] args) {
 	System.out.println("This is the 1st step to lern GitHUB");
 	System.out.println("Sucessfully Complete");
-	
+	System.out.println("Updated Sucessfully");
 }
 }
